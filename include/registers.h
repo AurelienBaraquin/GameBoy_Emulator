4e98af6ff537e332e5e6b8e7c10aef8a1b2a3b9a
aurelienBaraquin <aurelien.baraquin@epitech.eu>
2023-12-11 15:23:43 +0100

    Add registers.h header file With registers unamed structure for store all 16bit register and there own 8bit (from the same memory location) in this struct

---
+++ include/registers.h
@@ -0,0 +1,46 @@
+#pragma once
+
+struct registers {
+	struct {
+		union {
+			struct {
+				unsigned char f;
+				unsigned char a;
+			};
+			unsigned short af;
+		};
+	};
+	
+	struct {
+		union {
+			struct {
+				unsigned char c;
+				unsigned char b;
+			};
+			unsigned short bc;
+		};
+	};
+	
+	struct {
+		union {
+			struct {
+				unsigned char e;
+				unsigned char d;
+			};
+			unsigned short de;
+		};
+	};
+	
+	struct {
+		union {
+			struct {
+				unsigned char l;
+				unsigned char h;
+			};
+			unsigned short hl;
+		};
+	};
+	
+	unsigned short sp;
+	unsigned short pc;
+} extern registers;
