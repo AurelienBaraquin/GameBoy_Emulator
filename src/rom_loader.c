b6069ee35ab4bf2ef2fb90acfdb8d7f731d45504
aurelienBaraquin <aurelien.baraquin@epitech.eu>
2023-12-11 17:08:16 +0100

    Init variable for rom loading

--- src/rom_loader.c
+++ src/rom_loader.c
@@ -4,22 +4,16 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#define ROM_NAME_SIZE 0x10      // 16 bytes
+#define ROM_HEADER_SIZE 0x180   // 384 bytes
+
+char romName[ROM_NAME_SIZE + 1]; // +1 for \0 terminator
+enum romType romType;
+size_t romSize;
+size_t ramSize;
+u_int8_t header[ROM_HEADER_SIZE];
+
 u_int8_t loadROM(char *romPath)
 {
-    int fd = open(romPath, O_RDONLY);
-    if (fd == -1) {
-        return 1;
-    }
-    
-    struct stat st;
-    if (fstat(fd, &st) == -1) {
-        return 1;
-    }
-    
-    u_int8_t *rom = malloc(st.st_size);
-    if (read(fd, rom, st.st_size) == -1) {
-        return 1;
-    }
-    
-    close(fd);
+
 }
