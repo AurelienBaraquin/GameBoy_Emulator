b82b50f29f890f62a811e0cf21bf1ff686ce9513
aurelienBaraquin <aurelien.baraquin@epitech.eu>
2023-12-11 21:00:42 +0100

    Add ROM loading messages and display ROM information
    This commit adds ROM loading messages and displays ROM information such as name, type, size, and entry point after successfully loading the ROM. It also includes an error message if the ROM fails to load.

--- src/main.c
+++ src/main.c
@@ -9,6 +9,16 @@ int main(int ac, char **av)
         return 1;
     }
 
-    if (loadROM(av[1]) != 0)
+    printf("Loading ROM %s...\n", av[1]);
+
+    if (loadROM(av[1]) != 0) {
+        printf("Error loading ROM\n");
         return 1;
+    }
+
+    printf("ROM loaded successfully !\n");
+    printf("   Name:         %s\n", romHeader.title);
+    printf("   Type:         %s\n", romTypeString[romHeader.romType]);
+    printf("   Size:         %d Kb\n", romSize[romHeader.romType].bytes);
+    printf("   Entry point:  0x%X\n", romHeader.entryPoint);
 }
