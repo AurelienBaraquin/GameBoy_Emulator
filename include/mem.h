ded855d2710677382fafa7f81fc7d7996d65914f
aurelienBaraquin <aurelien.baraquin@epitech.eu>
2023-12-12 10:43:32 +0100

    Refactor memory handling to use a memoryBus struct

--- include/mem.h
+++ include/mem.h
@@ -4,10 +4,12 @@
 
 extern const u8 ioReset[0x100]; // Initial values for IO registers
 
-extern u8 cartridge[0x8000]; // 2MB of cartridge ROM
-extern u8 sram[0x2000]; // 8KB of SRAM
-extern u8 io[0x100]; // IO registers
-extern u8 vram[0x2000]; // 8KB of video RAM
-extern u8 oam[0x100]; // 256 bytes of OAM
-extern u8 wram[0x2000]; // 8KB of working RAM
-extern u8 hram[0x80]; // 128 bytes of high RAM
+extern struct memoryBus {
+    u8 cartridge[0x8000]; // 2MB of cartridge ROM
+    u8 sram[0x2000]; // 8KB of SRAM
+    u8 io[0x100]; // IO registers
+    u8 vram[0x2000]; // 8KB of video RAM
+    u8 oam[0x100]; // 256 bytes of OAM
+    u8 wram[0x2000]; // 8KB of working RAM
+    u8 hram[0x80]; // 128 bytes of high RAM
+} memoryBus;
--- src/rom_loader.c
+++ src/rom_loader.c
@@ -54,7 +54,7 @@ int loadROM(char *romPath)
     }
 
     rewind(f); // Go back to the beginning of the file
-    if (fread(&cartridge, fileSize, 1, f) != 1) {
+    if (fread(&memoryBus.cartridge, fileSize, 1, f) != 1) {
         printf("Error reading ROM\n");
         return 1;
     }
