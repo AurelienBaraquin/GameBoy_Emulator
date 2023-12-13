e3ed119711f244719da80d3f6d49b2c82de1f146
aurelienBaraquin <aurelien.baraquin@epitech.eu>
2023-12-13 17:26:22 +0100

    Update operation.h and register.c

--- src/initialiser/register.c
+++ src/initialiser/register.c
@@ -5,11 +5,17 @@ struct registers registers;
 
 void initializeRegisters(void)
 {
-    registers.af = AF_START; // A is unspecified, Flags are 0b10110000 (Z=0, N=0, H=1, C=1)
-    registers.bc = BC_START;
-    registers.de = DE_START;
-    registers.hl = HL_START;
-    registers.sp = SP_START;
-    registers.pc = PC_START;
-    registers.ie = IE_START;
+    registers.a = 0x01;
+    registers.fzero = 0x01;
+    registers.fsubtract = 0x00;
+    registers.fhalf_carry = 0x01;
+    registers.fcarry = 0x01;
+    registers.b = 0x00;
+    registers.c = 0x13;
+    registers.d = 0x00;
+    registers.e = 0xD8;
+    registers.h = 0x01;
+    registers.l = 0x4D;
+    registers.sp = 0xFFFE;
+    registers.pc = 0x0100;
 }
